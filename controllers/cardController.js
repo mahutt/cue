9eb0177cb533c3db56bc9d9b64c70fe4d780f382
mahutt <tommy.mahut@gmail.com>
2024-01-22 13:43:35 -0500

    Added update card page.

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -14,4 +14,24 @@ exports.create_card = asyncHandler(async (req, res, next) => {
     res.redirect(`/decks/${deck}`);
 });
 
-// @todo commit current status
+// Read a card.
+exports.read_card = asyncHandler(async (req, res, next) => {
+    const id = req.params.id;
+    const card = await Card.findById(id);
+    res.render('card/show', { card: card });
+});
+
+// Uppdate a card.
+exports.update_card = asyncHandler(async (req, res, next) => {
+    const { id, question, answer } = req.body;
+    const updatedCard = await Card.findOneAndUpdate(
+        { _id: id },
+        { $set: { question: question, answer: answer } },
+        { new: true }
+    );
+    if (updatedCard) {
+        res.redirect(`/decks/${updatedCard.deck}`);
+    } else {
+        throw new Error('Could not update card.');
+    }
+});
--- routes/cards.js
+++ routes/cards.js
@@ -4,7 +4,13 @@ var router = express.Router();
 // Require controller modules.
 const card_controller = require('../controllers/cardController');
 
-// Creating a deck.
+// Creating a card.
 router.post('/', card_controller.create_card);
+
+// Reading a card.
+router.get('/:id', card_controller.read_card);
+
+// Updating a card.
+router.post('/update', card_controller.update_card);
 
 module.exports = router;
