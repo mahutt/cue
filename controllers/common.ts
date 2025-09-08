a82d6e9fdcf0790569b059649caace8b1720564c
mahutt <tommy.mahut@gmail.com>
2025-09-08 19:26:24 -0400

    Rewrite common controller file in TS

--- controllers/common.ts
+++ controllers/common.ts
@@ -1,7 +1,10 @@
-exports.extractDepartmentAndNumber = function (courseCode) {
+export function extractDepartmentAndNumber(courseCode: string): {
+    department: string;
+    number: string;
+} | null {
     const match = courseCode.match(/^([A-Za-z]+)(\d+)$/);
     if (!match) return null;
     const department = match[1];
     const number = match[2];
     return { department, number };
-};
+}
