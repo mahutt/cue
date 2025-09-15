c749c451b8ee17837ed3ab3a4863e003eb7d0261
mahutt <tommy.mahut@gmail.com>
2025-09-14 23:45:12 -0400

    Convert userController to ts

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,49 +1,56 @@
-const User = require('../models/user');
-const Course = require('../models/course');
-const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcryptjs');
+import * as User from '../models/user';
+import asyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import { AuthenticatedRequest } from './types';
+import { Response } from 'express';
 
-exports.allUsers = asyncHandler(async (req, res, _) => {
+exports.allUsers = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
     // since users don't have a roles attribute, I hardcode my username for now
     if (!req.user || req.user.name !== 'mahutt') {
-        return res.sendStatus(401);
+        res.sendStatus(401);
+        return;
     }
     const users = await User.all();
     res.send(users);
 });
 
-exports.allUserNames = asyncHandler(async (req, res, next) => {
+exports.allUserNames = asyncHandler(async (_, res) => {
     const users = await User.allNames();
     res.send(users);
 });
 
-exports.create_user = asyncHandler(async (req, res, _) => {
+exports.create_user = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
     try {
         // since users don't have a roles attribute, I hardcode my username for now
         if (!req.user || req.user.name !== 'mahutt') {
-            return res.sendStatus(401);
+            res.sendStatus(401);
+            return;
         }
 
         const { name, password } = req.body;
 
         if (!name || !password) {
-            return res.status(400).send({ error: 'Name and password are required.' });
+            res.status(400).send({ error: 'Name and password are required.' });
+            return;
         }
 
         const hash = await bcrypt.hash(password, 10);
         try {
             await User.register({ name, hash });
-        } catch (e) {
+        } catch (e: any) {
             if (e.code === 'SQLITE_CONSTRAINT' && e.errno === 19) {
                 res.status(409).send({ error: 'Username already exists.' });
+                return;
             } else {
                 res.status(500).send({ error: 'Unknown error occurred.' });
+                return;
             }
         }
 
         const newUser = await User.findByName(name);
         if (!newUser) {
-            return res.status(500).send({ error: 'User creation failed.' });
+            res.status(500).send({ error: 'User creation failed.' });
+            return;
         }
 
         res.status(201).send({ message: 'User created successfully.', user: { id: newUser.id, name: newUser.name } });
