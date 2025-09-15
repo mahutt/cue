7b13a193da91708fa982a4ee3b69fc4dd1461b4d
mahutt <tommy.mahut@gmail.com>
2025-09-15 00:20:16 -0400

    Delete old web components

--- public/javascripts/components/menuOptions.component.js
+++
@@ -1,8 +0,0 @@
-class MenuOptions extends HTMLElement {
-    connectedCallback() {
-        const menuBar = document.querySelector('menu-bar');
-        menuBar.setMenuOptions(this.children);
-    }
-}
-
-customElements.define('menu-options', MenuOptions);
