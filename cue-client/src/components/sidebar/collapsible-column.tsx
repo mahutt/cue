9b7314d9093ebbb4d5f3dd26b2fa71707e99d78a
mahutt <tommy.mahut@gmail.com>
2025-10-13 14:13:44 -0400

    Try dvh for sidebar height

--- cue-client/src/components/sidebar/collapsible-column.tsx
+++ cue-client/src/components/sidebar/collapsible-column.tsx
@@ -11,7 +11,7 @@ export default function CollapsibleColumn() {
     const { isSidebarOpen, closeSidebar } = useSidebar();
     return (
         <div
-            className="z-10 absolute l-0 h-full bg-white md:static shadow"
+            className="z-10 absolute l-0 h-dvh bg-white md:static shadow"
             style={{ overflow: 'hidden', transition: 'all 0.2s ease-out', width: isSidebarOpen ? 260 : 0 }}
         >
             <div className="flex flex-col w-[260px] max-h-screen overflow-y-auto p-3">
